dbc52b4b1a776277b1fdcde12feb171704d44c85
kiketion <kiketionje@gmail.com>
2022-04-07 17:30:04 +0300

    api

--- App.js
+++ App.js
@@ -1,20 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, Text } from 'react-native';
 
-export default function App() {
+const App = () => {
+  const [coins, setCoins] = useState([]);
+
+  const loadData = async () => {
+    const res = await fetch(
+      'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false'
+    );
+    const data = await res.json();
+    setCoins(data);
+  };
+
+  useEffect(() => {
+    loadData();
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>kiketion first React Native App!</Text>
-      <StatusBar style='auto' />
+    <View>
+      <Text>Hello World!! </Text>
     </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
